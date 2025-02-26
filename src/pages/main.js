c143daa55793f9c668864481ef8fa74d4d952cbf
XandyGomes <x3.computadores@hotmail.com>
2025-02-25 22:11:35 -0300

    Aula.25.02.2025

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
-import { Container, Form, Input, SubmitButton } from "../styles";
+import { Keyboard, ActivityIndicator } from "react-native";
 import Icon from "@expo/vector-icons/MaterialIcons";
+import api from "../services/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from "../styles";
 
 export default class Main extends Component {
+  state = {
+    newUser: "",
+    user: [],
+    loading: false,
+  };
+
+  async componentDidMount(){
+    const users = await AsyncStorage.getItem('users')
+    if(users) {
+      this.setState({ users: JSON.parse(users)})
+    }
+  }
+
+  // componentDidUpdate(_, prevState){
+  //   const { users } = this.setState
+  //   if(prevState.users !== users)
+  // }
+
+
   render() {
     return (
       <Container>
@@ -12,9 +46,9 @@ export default class Main extends Component {
             autoCapitalize="none" // Não deixa a primeira letra maiúscula
             placeholder="Adicionar usuário" // Texto que aparece no input
           />
-            <SubmitButton>
-                <Icon name="add" size={22} color="#FFF" />
-            </SubmitButton>
+          <SubmitButton>
+            <Icon name="add" size={22} color="#FFF" />
+          </SubmitButton>
         </Form>
       </Container>
     );
