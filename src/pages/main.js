4d500780487d375d4caad195273d74cc10276daf
XandyGomes <x3.computadores@hotmail.com>
2025-03-11 21:04:36 -0300

    Aula.11.03.2025

--- src/pages/main.js
+++ src/pages/main.js
@@ -24,20 +24,55 @@ export default class Main extends Component {
     loading: false,
   };
 
-  async componentDidMount(){
-    const users = await AsyncStorage.getItem('users')
-    if(users) {
-      this.setState({ users: JSON.parse(users)})
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem("users");
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
     }
   }
 
-  // componentDidUpdate(_, prevState){
-  //   const { users } = this.setState
-  //   if(prevState.users !== users)
-  // }
+  componentDidUpdate(_, prevState) {
+    const { users } = this.setState;
+    if (prevState.users !== users) {
+      AsyncStorage.setItem("users", JSON.stringify(users));
+    }
+  }
 
+  handleAddUser = async () => {
+    try {
+      const { users, newUser } = this.state;
+      this.setState({ loading: true });
+
+      const response = await api.get(`/users/${newUser}`);
+      if (users.find((user) => user.login === response.data.login)) {
+        alert("Usuário já adicionado!");
+        this.setState({ loading: false });
+        return;
+      }
+      console.log(response)
+      const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+      };
+
+      console.log(data);
+      
+      this.setState({
+        users: [...users, data],
+        newUser: "",
+        loading: false,
+      });
+      Keyboard.dismiss();
+    } catch (error) {
+      alert("Usuário não encontrado");
+      this.setState({ loading: false });
+    }
+  };
 
   render() {
+    const { users, newUser, loading } = this.state;
     return (
       <Container>
         <Form>
@@ -45,9 +80,17 @@ export default class Main extends Component {
             autoCorrect={false} //  Não corrige o texto
             autoCapitalize="none" // Não deixa a primeira letra maiúscula
             placeholder="Adicionar usuário" // Texto que aparece no input
+            value={newUser}
+            onChangeText={(text) => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton>
-            <Icon name="add" size={22} color="#FFF" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={22} color="#FFF" />
+            )}
           </SubmitButton>
         </Form>
       </Container>
